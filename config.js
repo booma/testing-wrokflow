213a6d4877db5b07f5db3f9b69667934eb3039e0
booma <booma.radhakrishnan@gmail.com>
2023-06-06 00:31:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-"schedule": "*/1 * * * *",
 "interval": 0,
 "forkOnly": false,
 "closureComment": "The pr is opened for 2 days and not having any actions and closing this pr and please recreate the pr."
